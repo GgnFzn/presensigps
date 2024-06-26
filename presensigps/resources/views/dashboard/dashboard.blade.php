dcedf55d74886661268be0e49dec57227db1f6be
GgnFzn <162352596+GgnFzn@users.noreply.github.com>
2024-06-27 04:13:42 +0700

    membuat leaderboard

--- presensigps/resources/views/dashboard/dashboard.blade.php
+++ presensigps/resources/views/dashboard/dashboard.blade.php
@@ -7,7 +7,7 @@
                 </div>
                 <div id="user-info">
                     <h2 id="user-name">{{ Auth::guard('karyawan')->user()->nama_lengkap }}</h2>
-                    <span id="user-role">Head of IT</span>
+                    <span id="user-role">{{ Auth::guard('karyawan')->user()->jabatan }}</span>
                 </div>
             </div>
         </div>
@@ -102,7 +102,8 @@
                     <div class="col-3">
                         <div class="card">
                             <div class="card-body text-center" style="padding: 12px 12px !important; line-heigt: 0.8rem">
-                                <span class="badge bg-danger" style="position: absolute; top: 3px; right: 10px; font-size: 0.6rem; z-index: 999">10</span>
+                                <span class="badge bg-danger" style="position: absolute; top: 3px; right: 10px; font-size: 0.6rem; z-index: 999">
+                            {{ $rekappresensi->jmlhadir }}</span>
                                 <ion-icon name="accessibility-outline" style="font-size: 1.6rem" class="text-primary"></ion-icon>
                                 <br>
                                 <span style="font-size: 0.8rem; font-weight: 500">Hadir</span>
@@ -132,7 +133,8 @@
                     <div class="col-3">
                         <div class="card">
                             <div class="card-body text-center" style="padding: 12px 12px !important; line-heigt: 0.8rem">
-                                <span class="badge bg-danger" style="position: absolute; top: 3px; right: 10px; font-size: 0.6rem; z-index: 999">10</span>
+                                <span class="badge bg-danger" style="position: absolute; top: 3px; right: 10px; font-size: 0.6rem; z-index: 999">
+                                {{ $rekappresensi->jmlterlambat }}</span>
                                 <ion-icon name="alarm-outline" style="font-size: 1.6rem" class="text-danger"></ion-icon>
                                 <br>
                                 <span style="font-size: 0.8rem; font-weight: 500">Telat</span>
@@ -182,48 +184,23 @@
                     </div>
                     <div class="tab-pane fade" id="profile" role="tabpanel">
                         <ul class="listview image-listview">
+                            @foreach($leaderboard as $d)
                             <li>
                                 <div class="item">
                                     <img src="assets/img/sample/avatar/avatar1.jpg" alt="image" class="image">
                                     <div class="in">
-                                        <div>Edward Lindgren</div>
-                                        <span class="text-muted">Designer</span>
+                                        <div>
+                                            <b>{{ $d->nama_lengkap }}</b> <br>
+                                            <small class="text-muted">{{ $d->jabatan }}</small>
+                                        </div>
+                                        <span class="badge {{ $d->jam_in < "07:00" ? "bg-success" : "bg-danger"}}">
+                                            {{ $d->jam_in }}
+                                        </span>
                                     </div>
                                 </div>
                             </li>
-                            <li>
-                                <div class="item">
-                                    <img src="assets/img/sample/avatar/avatar1.jpg" alt="image" class="image">
-                                    <div class="in">
-                                        <div>Emelda Scandroot</div>
-                                        <span class="badge badge-primary">3</span>
-                                    </div>
-                                </div>
-                            </li>
-                            <li>
-                                <div class="item">
-                                    <img src="assets/img/sample/avatar/avatar1.jpg" alt="image" class="image">
-                                    <div class="in">
-                                        <div>Henry Bove</div>
-                                    </div>
-                                </div>
-                            </li>
-                            <li>
-                                <div class="item">
-                                    <img src="assets/img/sample/avatar/avatar1.jpg" alt="image" class="image">
-                                    <div class="in">
-                                        <div>Henry Bove</div>
-                                    </div>
-                                </div>
-                            </li>
-                            <li>
-                                <div class="item">
-                                    <img src="assets/img/sample/avatar/avatar1.jpg" alt="image" class="image">
-                                    <div class="in">
-                                        <div>Henry Bove</div>
-                                    </div>
-                                </div>
-                            </li>
+                            @endforeach
+                            
                         </ul>
                     </div>
 
